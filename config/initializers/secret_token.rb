18934ef011823eee7c2418801c1c736f76972f0f
Martin Bianculli <mbianculli-c@g2llc.com>
2012-01-10 23:33:55 -0300

    Proyecto Rails inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClickInmobiliario::Application.config.secret_token = 'aba5e71f81d86ca7d00d524229e5ca246f532667c3ac3573314d26354bd7ea41425889b3307151074e224eb3b76b84e96763d099bb52c042a89adcb266c94560'
