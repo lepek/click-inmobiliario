5d525210012114b38bcec3aa4a38cc7e83487224
leotioni <leotioni@gmail.com>
2012-02-06 21:30:18 -0300

    Adding Real Estate deletion support

--- app/controllers/admin/real_estates_controller.rb
+++ app/controllers/admin/real_estates_controller.rb
@@ -74,11 +74,14 @@ module Admin
     # DELETE /real_estates/1.json
     def destroy
       @real_estate = RealEstate.find(params[:id])
+      real_estate_name = @real_estate.name
       @real_estate.destroy
 
       respond_to do |format|
-        format.html { redirect_to real_estates_url }
-        format.json { head :ok }
+        format.html do
+          flash[:success] = "La inmobiliaria #{real_estate_name.upcase} fue eliminada correctamente." 
+          redirect_to admin_root_path(:anchor => 'real-estates')
+        end
       end
     end
   end
